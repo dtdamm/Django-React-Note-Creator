72a8768f2ffba065463f4f9e2252da997a42765f
Ultimate553 <diego.torred@gmail.com>
2025-02-12 14:10:35 -0600

    Finished local development

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,22 +2,23 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
 
-# Serailizer for validates user model fields
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {
-            "password": {"write_only": True}} # Accetp password when creating user but dont return it when a user is fetched
-    
+        extra_kwargs = {"password": {"write_only": True}}  # Accetp password when creating user but dont return it when a user is fetched
+        
     # If user is valid, create a user object
     def create(self, validated_data):
         print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
 
-class NoteSerializer(serializers.Serializer):
+
+class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fileds = ["id", "title", "content", "created_at", "author"]
+        fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
+
